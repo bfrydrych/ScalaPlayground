06d13344e8b468839f98859c7c562529dbd0c83c
bfrydrych <b_frydrych@op.pl>
2013-04-01 23:37:29 +0200

    balance parenthesis

--- src/main/scala/com/github/bfrydrych/scala/Play.scala
+++ src/main/scala/com/github/bfrydrych/scala/Play.scala
@@ -10,4 +10,18 @@ object Play {
 	    if (column == row) return 1
 	    pascalTriangle(column -1, row -1) + pascalTriangle(column, row -1)
 	}
+	
+	def isParenthesisBalanced(chars: List[Char]): Boolean = {
+	  def evaluate(chars: List[Char], openCount: Int): Int = {
+		  if (chars.isEmpty) return openCount
+		  val head = chars.head
+		  var count = openCount
+		  if (head == '(') count += 1
+		  if (head == ')' && count == 0) return -1 
+		  if (head == ')')  count -= 1
+		  evaluate(chars.tail, count)
+	  }
+	  val openCount = evaluate(chars, 0)
+	  return openCount == 0
+	}
 }
